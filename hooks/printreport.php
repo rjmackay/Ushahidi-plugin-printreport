05faf9b3850c5b4b39be4cf710bb8250f140e8e8
Robbie Mackay <rm@robbiemackay.com>
2012-06-13 09:34:02 +1200

    initial commit - extracting code from RCA deployment

---
+++ hooks/printreport.php
@@ -0,0 +1,39 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Print Report Hook - Load All Events
+ *
+ * PHP version 5
+ * LICENSE: This source file is subject to LGPL license 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ * @author	   Ushahidi Team <team@ushahidi.com> 
+ * @package	   Ushahidi - http://source.ushahididev.com
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license	   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ */
+
+class printreport {
+	
+	/**
+	 * Registers the main event add method
+	 */
+	public function __construct()
+	{
+		// Hook into routing
+		Event::add('system.pre_controller', array($this, 'add'));
+	}
+	
+	/**
+	 * Adds all the events to the main Ushahidi application
+	 */
+	public function add()
+	{
+		// Only add the events if we are on that controller
+		if (Router::$controller == 'reports' AND (Router::$method == 'view' OR Router::$method == 'index'))
+		{
+			plugin::add_stylesheet('printreport/css/print');
+		}
+	}
+}
+
+new printreport;
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== About ===
+name: Print Report
+website: http://www.ushahidi.com
+description: Add print media css to reports via pages
+version: 0.1
+requires: 2.3
+tested up to: 2.3
+author: Robbie Mackay
+
+== Description ==
+Printable reports. Built to work with unicorn / internewscar theme
+
+== Installation ==
+1. Copy the entire /printreport/ directory into your /plugins/ directory.
+2. Activate the plugin.
+
+== Changelog ==
+
